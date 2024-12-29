d568f803aff85a86af78dbb3ea62b2705b166b9e
Phonedolly <Phonedolly@users.noreply.github.com>
2024-12-30 00:05:36 +0900

    chore: apply foreground color for <p />

--- components/mdx/p.tsx
+++ components/mdx/p.tsx
@@ -1,6 +1,6 @@
 export default function P({ children }: { children: React.ReactNode }) {
   return (
-    <p className="mt-4 text-base sm:text-lg leading-relaxed text-secondary">
+    <p className="mt-4 text-base sm:text-lg leading-relaxed text-foreground">
       {children}
     </p>
   );
