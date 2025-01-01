e078722c13df040056f35ca417a97dc793d73000
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-01 10:28:08 +0900

    BREAKING CHANGE: move actual mdx file into child of directory

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -1,7 +1,8 @@
 import * as fs from "fs/promises";
 import * as path from "path";
 
-import Container from "./container";
+import PostContainer from "./post-container";
+import { Metadata } from "next";
 
 interface PostPageProps {
   params: Promise<{
@@ -11,13 +12,13 @@ interface PostPageProps {
 
 export default async function Page({ params }: PostPageProps) {
   const postId = (await params).postId;
-  const page = await import(`@/posts/${postId}.mdx`);
+  const page = await import(`@/posts/${postId}/${postId}.mdx`);
   const { default: Post, frontmatter } = page;
 
   return (
-    <Container frontmatter={frontmatter}>
+    <PostContainer frontmatter={frontmatter}>
       <Post />
-    </Container>
+    </PostContainer>
   );
 }
 
@@ -32,9 +33,9 @@ export const dynamicParams = false;
 
 export async function generateMetadata({ params }: PostPageProps) {
   const postId = (await params).postId;
-  const { frontmatter } = await import(`@/posts/${postId}.mdx`);
+  const { frontmatter } = await import(`@/posts/${postId}/${postId}.mdx`);
   return {
     title: frontmatter.title,
     description: frontmatter.description,
-  };
+  } as Metadata;
 }
