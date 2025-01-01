38c4589f56c51ed93c6f57b58fe80cef206c92b0
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-01 10:25:05 +0900

    refactor: rename container.tsx to post-container.tsx

--- app/post/[postId]/post-container.tsx
+++ app/post/[postId]/post-container.tsx
@@ -1,8 +1,9 @@
-import { PostMetadata } from "@/types/post-metadata";
+import { epochToDateString } from "@/lib/epoch-to-datestring";
+import { IFrontmatter } from "@/types/IFrontmatter";
 
 type ContainerProps = {
   children: React.ReactNode;
-  frontmatter: Promise<PostMetadata>;
+  frontmatter: Promise<IFrontmatter>;
 };
 
 type UpdateDateProps = {
@@ -27,18 +28,11 @@ const Description = ({ description }: { description: string }) => {
 };
 
 const UpdateDate = ({ createdAt, updatedAt }: UpdateDateProps) => {
-  const createdDate = new Date(createdAt).toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-  const updatedDate = new Date(updatedAt).toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  const createdDate = epochToDateString(createdAt);
+  const updatedDate = epochToDateString(updatedAt);
+
   return (
-    <div className="flex flex-col py-0.5 md:py-1">
+    <div className="flex w-1/2 max-w-[30vw] flex-col border-b border-foreground">
       <p className="font-geist text-sm font-normal tracking-tight text-foreground sm:text-base">
         {createdDate}에 작성
       </p>
@@ -46,35 +40,34 @@ const UpdateDate = ({ createdAt, updatedAt }: UpdateDateProps) => {
         <p className="font-geist text-sm font-normal tracking-tight text-foreground sm:text-base">
           {updatedDate}에 업데이트
         </p>
-        <Divider />
+        {/* <Divider /> */}
       </div>
     </div>
   );
 };
 
-const Divider = () => {
-  return <div className="relative bottom-px h-px w-full bg-foreground"></div>;
-};
-
-const Info = ({ frontmatter }: { frontmatter: PostMetadata }) => {
+const Info = ({ frontmatter }: { frontmatter: IFrontmatter }) => {
   const { description, createdAt, updatedAt } = frontmatter;
 
   return (
-    <div className="flex flex-col py-0.5 md:gap-y-0.5 md:py-1">
+    <div className="flex flex-col gap-y-0.5 md:gap-y-1">
       <Description description={description} />
-      <UpdateDate createdAt={createdAt} updatedAt={updatedAt} />
+      <UpdateDate
+        createdAt={createdAt}
+        updatedAt={updatedAt.at(-1) ?? createdAt}
+      />
     </div>
   );
 };
 
-export default async function Container({
+export default async function PostContainer({
   children,
   frontmatter,
 }: ContainerProps) {
   const { title } = await frontmatter;
 
   return (
-    <div className="relative flex w-full flex-col gap-y-0 py-1 md:gap-y-1.5">
+    <div className="relative flex w-full flex-col gap-y-0 md:gap-y-1.5 md:py-1">
       <div className="flex flex-col gap-y-0 md:gap-y-0.5">
         <Title title={title} />
         <Info frontmatter={await frontmatter} />
