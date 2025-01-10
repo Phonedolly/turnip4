ddcb1015d6e7fbb5854699e97de0755abfbae585
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-11 02:39:48 +0900

    chore: adjust some layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
             "mx-auto flex w-full flex-col items-center justify-center",
             "my-6 gap-y-2 px-5",
             "sm:my-8 sm:gap-y-4 sm:px-12",
-            "max-w-screen-lg md:my-16 md:gap-y-3.5 md:px-12",
+            "max-w-screen-lg md:my-16 md:gap-y-3.5 md:px-20",
             "selection:bg-bone-500 selection:text-foreground",
           )}
         >
