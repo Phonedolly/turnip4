7888d482a2ce484b3bbd6f970322d917e219258c
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-13 02:56:43 +0900

    chore: adjust p tag layout

--- components/mdx/p.tsx
+++ components/mdx/p.tsx
@@ -2,13 +2,13 @@ export default function P({ children }: { children: React.ReactNode }) {
   /* check if children is actually a text node */
   if (typeof children === "string") {
     return (
-      <p className="mt-0 text-base leading-relaxed text-foreground md:mt-0 md:leading-loose lg:mt-4">
+      <p className="mt-0 text-base leading-relaxed text-foreground md:mt-0 md:leading-loose lg:mt-1">
         {children}
       </p>
     );
   }
   return (
-    <div className="mt-2 text-base leading-relaxed text-foreground md:mt-0 md:leading-loose lg:mt-4">
+    <div className="mt-2 text-base leading-relaxed text-foreground md:mt-0 md:leading-loose lg:mt-1">
       {children}
     </div>
   );
