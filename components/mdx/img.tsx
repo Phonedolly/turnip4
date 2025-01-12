dbc997fecf5ee79afdd025d0303efbdab1d4f5f9
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-13 01:23:52 +0900

    refactor: improve image load logic

--- components/mdx/img.tsx
+++ components/mdx/img.tsx
@@ -1,48 +1,29 @@
-import fs from "fs/promises";
+import fs from "fs";
+import Image from "next/image";
+import path from "path";
 import probe from "probe-image-size";
-
-type ProportionalWidth = number;
-function isProportionalWidth(width?: number): width is ProportionalWidth {
-  return (
-    width !== null &&
-    width !== undefined &&
-    typeof width === "number" &&
-    width >= 0 &&
-    width <= 1
-  );
-}
 
 type ImgProps = {
   src: string;
   alt: string;
-  width?: ProportionalWidth;
 };
 
-export default async function Img({ src: _src, alt, width }: ImgProps) {
-  if (width && isProportionalWidth(width) !== true) {
-    throw new Error("Invalid width");
-  }
-  const pathName = _src.split("/")[1];
-  const imageName = _src.split("/")[2];
+export default async function Img({ src, alt }: ImgProps) {
+  const postId = src.split("/")[2];
+  const imageName = src.split("/")[3];
 
-  const imageData = await fs.readFile(
-    `${process.cwd()}/posts/${pathName}/${imageName}`,
+  const imageStream = fs.createReadStream(
+    path.join(process.cwd(), `posts/${postId}/${imageName}`),
   );
-  const imageSize = probe.sync(imageData);
-  if (imageSize === null) {
-    throw new Error(`[Img] ${imageName} is invalid or not an image`);
-  }
-  const dataUrl = `data:${imageSize.mime};base64,${imageData.toString("base64")}`;
+  const { width, height, mime } = await probe(imageStream);
+  const imageBuffer = fs.readFileSync(
+    path.join(process.cwd(), "posts", postId, imageName),
+  );
+  const dataUri = `data:${mime};base64,${imageBuffer.toString("base64")}`;
 
-  // TODO: add lazy loading
   return (
-    <div className="flex w-full flex-row justify-center">
-      <img
-        src={dataUrl}
-        alt={alt}
-        width={imageSize.width}
-        height={imageSize.height}
-      />
+    <div className="my-4 flex w-full flex-row justify-center">
+      <Image src={dataUri} alt={alt} width={width} height={height} />
     </div>
   );
 }
