b5864d92e7097eba4833f4a49a78028570e30773
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-12 22:53:13 +0900

    chore: adjust h2 font size

--- components/mdx/h2.tsx
+++ components/mdx/h2.tsx
@@ -1,6 +1,6 @@
 export default function H2({ children }: { children: React.ReactNode }) {
   return (
-    <h2 className="mt-6 sm:mt-8 text-2xl sm:text-3xl lg:text-4xl font-semibold text-black dark:text-white">
+    <h2 className="mt-6 sm:mt-8 text-2xl sm:text-2xl lg:text-3xl font-semibold text-black dark:text-white">
       {children}
     </h2>
   );
