120d691d43033a11b85bf667ff2970568634451e
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-13 01:23:31 +0900

    feat: inject postId to src of image node at (mdx) compile time

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import rehypeKatex from "rehype-katex";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { visit } from "unist-util-visit";
 
 import createMDX from "@next/mdx";
 
@@ -14,7 +15,41 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter, remarkGfm],
-    rehypePlugins: [[rehypeKatex, { strict: true, throwOnError: true }]],
+    rehypePlugins: [
+      [rehypeKatex, { strict: true, throwOnError: true }],
+      () => (tree) => {
+        let postId = "";
+        visit(tree, "root", (node) => {
+          visit(node, "mdxjsEsm", (node) => {
+            const estree = node.data.estree.body.at(0);
+            if (!estree) {
+              return;
+            }
+            visit(estree, "ExportNamedDeclaration", (node) => {
+              const variableDeclarator = node.declaration.declarations.at(0);
+              if (!variableDeclarator) {
+                return;
+              }
+              const objectExpression = variableDeclarator.init.properties;
+              for (const property of objectExpression) {
+                if (property.key.value === "titleId") {
+                  postId = property.value.value;
+                }
+              }
+            });
+          });
+        });
+
+        visit(tree, "element", (node) => {
+          if (node.tagName === "img") {
+            const src = node.properties.src.replace(/^\.\//, "");
+            const newSrc = `/posts/${postId}/${src}`;
+            node.properties.src = newSrc;
+          }
+        });
+        return tree;
+      },
+    ],
   },
 });
 
