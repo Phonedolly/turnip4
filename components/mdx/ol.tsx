a5f6de8c4d88014bc67ac0c53198e30f4ad1c4a3
Phonedolly <Phonedolly@users.noreply.github.com>
2025-01-12 22:53:37 +0900

    chore: change font color of <ol /> to foreground

--- components/mdx/ol.tsx
+++ components/mdx/ol.tsx
@@ -1,6 +1,6 @@
 export default function OL({ children }: { children: React.ReactNode }) {
   return (
-    <ol className="mt-4 list-decimal list-inside space-y-2 text-secondary">
+    <ol className="mt-4 list-decimal list-inside space-y-2 text-foreground">
       {children}
     </ol>
   );
