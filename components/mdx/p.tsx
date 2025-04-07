6560d65564f0cde73d4bbf22ff3af8762e0c1738
Phonedolly <Phonedolly@users.noreply.github.com>
2025-04-07 23:26:04 +0900

    chore: adjust <p /> font size

--- components/mdx/p.tsx
+++ components/mdx/p.tsx
@@ -3,19 +3,16 @@ import { Children } from "react";
 export default function P({ children }: { children: React.ReactNode }) {
   /* if it contains only one child AND it's not a text node, it may be block element */
   const isBlock =
-    Children.toArray(children).length === 1 &&
-    typeof children !== "string";
+    Children.toArray(children).length === 1 && typeof children !== "string";
 
   if (isBlock) {
     return (
-      <div className="mt-2 text-lg leading-relaxed text-foreground md:leading-loose">
+      <div className="mt-2 text-base leading-relaxed text-foreground md:leading-loose">
         {children}
       </div>
     );
   }
   return (
-    <p className="mt-2 text-lg leading-relaxed text-foreground">
-      {children}
-    </p>
+    <p className="mt-2 text-base leading-loose text-foreground">{children}</p>
   );
 }
