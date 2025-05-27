5eda0c43784fd6b77f486d49cda3460f3a88b605
Phonedolly <Phonedolly@users.noreply.github.com>
2025-05-27 21:22:24 +0900

    chore: adjust margin-top of <ul/>

--- components/mdx/ul.tsx
+++ components/mdx/ul.tsx
@@ -1,7 +1,7 @@
 export default function UL({ children }: { children: React.ReactNode }) {
   return (
-    <ul className="mt-4 list-disc list-inside space-y-2 text-foreground">
+    <ul className="mt-2 list-disc list-inside space-y-2 text-foreground">
       {children}
     </ul>
   );
-} 
+}
