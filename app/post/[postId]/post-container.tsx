537783f1000bd56293438c11aa3f44c5ab614e26
Phonedolly <Phonedolly@users.noreply.github.com>
2025-05-27 22:41:27 +0900

    chore: adjust text sizes of layout

--- app/post/[postId]/post-container.tsx
+++ app/post/[postId]/post-container.tsx
@@ -21,7 +21,7 @@ const Title = ({ title }: { title: string }) => {
 
 const Description = ({ description }: { description: string }) => {
   return (
-    <p className="text-xl font-normal text-foreground md:text-2xl lg:text-2xl">
+    <p className="text-lg font-normal text-foreground md:text-lg">
       {description}
     </p>
   );
@@ -50,7 +50,7 @@ const Info = ({ frontmatter }: { frontmatter: IFrontmatter }) => {
   const { description, createdAt, updatedAt } = frontmatter;
 
   return (
-    <div className="flex flex-col gap-y-0.5 md:gap-y-1">
+    <div className="flex flex-col md:gap-y-0.5">
       <Description description={description} />
       <UpdateDate
         createdAt={createdAt}
--- components/header.tsx
+++ components/header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Stardue128Identity = () => {
   return (
-    <Link href="/" className="relative w-fit group flex flex-col">
-      <h1 className="text-3xl sm:text-4xl font-normal font-geist tracking-tight text-foreground hover:text-primary select-none">
+    <Link href="/" className="group relative flex w-fit flex-col">
+      <h1 className="select-none font-roboto text-3xl font-black tracking-tight text-foreground hover:text-primary sm:text-4xl">
         stardue128
       </h1>
       {/* <div className="group-hover:bg-foreground w-full h-0.5 absolute -bottom-px ml-[2px]"></div> */}
@@ -21,7 +21,7 @@ const MenuItem = ({
   return (
     <Link
       href={href}
-      className="text-foreground font-semibold tracking-tight text-base sm:text-lg hover:text-primary"
+      className="text-sm font-semibold tracking-tight text-foreground hover:text-primary sm:text-base"
     >
       {children}
     </Link>
@@ -30,7 +30,7 @@ const MenuItem = ({
 
 const Menu = () => {
   return (
-    <div className="w-full flex flex-row space-x-3 sm:space-x-4 md:space-x-6 justify-end items-end">
+    <div className="flex w-full flex-row items-end justify-end space-x-2 sm:space-x-3 md:space-x-4">
       <MenuItem href="/">처음</MenuItem>
       <MenuItem href="/all-posts">모두 보기</MenuItem>
     </div>
@@ -39,7 +39,7 @@ const Menu = () => {
 
 export default function Header() {
   return (
-    <header className="w-full py-0.5 md:py-0.5 flex flex-row justify-between border-b-[1.5px] border-foreground">
+    <header className="flex w-full flex-row justify-between border-b-[1.5px] border-foreground py-0.5 md:py-0.5">
       <Stardue128Identity />
       <Menu />
     </header>
